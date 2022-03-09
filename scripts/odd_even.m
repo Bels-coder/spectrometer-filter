ae671dee2808bf221efdcad0996b4f085e033ebf
chiarless <72812518+chiarless@users.noreply.github.com>
2022-03-09 23:04:49 +0100

    Update odd_even.m

--- scripts/odd_even.m
+++ scripts/odd_even.m
@@ -22,21 +22,25 @@
 
 %%
 
+wvl = dlmread('JIRAM_WVL_00.txt');                               % UPLOAD 'JIRAM_WVL_00.txt' FILE
+oe = 1:1:size(wvl); 
+par = oe(mod(oe,2)~=1);
+dis = oe(mod(oe,2)~=0);
 
-wave = ;                               % ADD WAVELENGTHS from file 'JIRAM_WVL_00.txt'
-oe = double(wvl);
-par = rem(oe, 2) == 0;
-dis = rem(oe, 2) == 1;
-spectrum = double(256,336) ;           % SELECT THE SLIT
+% Sample file
+filename = 'JM0071_limbs_00193.txt'; 
+file = fopen(filename);
+spectrum = textscan(file,'%f %f %f','HeaderLines',21);
+spectrum = cell2mat(spectrum);
+fclose(file);
+signal = spectrum(:,2);                                                   
+%
 
-for i = 0,size(spectrum(256,:)) 
+sp = signal.';
+spd = spline(wvl(dis), sp(dis), wvl);
+spp = spline(wvl(par), sp(par),  wvl);
+sp = 0.5*(spd+spp);
+signal_corrected = sp;
 
- sp = (spectrum(i,:));
- spp = spline(sp(par), wave(par), wave);
- spd = spline(sp(dis), wave(dis), wave);
- sp = 0.5*(spd+spp);
- spectrum(i, :) = sp;
- 
-end
 
 
