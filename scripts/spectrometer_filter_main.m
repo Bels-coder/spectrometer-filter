11b1cd1bea02fcb97dbd018dd58a556fbd0ab691
Bels-coder <78752329+Bels-coder@users.noreply.github.com>
2022-03-09 15:16:16 +0100

    Create spectrometer_filter_main.m

---
+++ scripts/spectrometer_filter_main.m
@@ -0,0 +1,21 @@
+% This script is the 'main' caller, which contains the call to each step of the filter.
+% The steps that has to be taken are the following:
+%
+% The program must filter a single sequence of a chosen orbit at a time.
+%
+% = 1 =
+  % Upload data
+  %
+% = 2 =
+  % Oddeven
+  %
+% = 3 =
+  % Negative values removal
+  %
+% = 4 =
+  % Spike removal
+  %
+% = 5 =
+  % RMS filter
+  %
+  
