aab24abf408f1d6300a3d1f8a9aa3aab52b892b5
chiarless <72812518+chiarless@users.noreply.github.com>
2022-03-30 12:27:54 +0200

    Update trms.m

--- functions/trms.m
+++ functions/trms.m
@@ -1,6 +1,17 @@
 function x = trms(a)
-
- % Return the root mean square of all the elements of *a*, flattened out.
-   x = sqrt(mean(abs(a).^2));
+% ===============================
+% Return the root mean square of all the elements of *s*, flattened out.
+%
+% SYNTAX :
+%	- time_rms = trms(s)
+%
+% INPUT :
+%	- s: is the signal in the time domain.
+%
+% OUTPUT:
+%	- time_rms: root-mean-square value of the signal.
+% ===============================
+ 
+   time_rms = sqrt(mean(abs(s).^2));
    
 end
