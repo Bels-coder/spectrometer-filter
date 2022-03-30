1f01e38f158581fd6edc2bc68c0f13523200c282
chiarless <72812518+chiarless@users.noreply.github.com>
2022-03-30 12:22:23 +0200

    Update frms.m

--- functions/frms.m
+++ functions/frms.m
@@ -1,8 +1,17 @@
 function [ rms ] = frms( S )
-
-    %FRMS uses Parseval's theorem to calculate the RMS of a signal 
-    %by examining the frequency domain of the signal.
-    %S is the signal in the frequency domain, i.e. fft(s)
+% ===============================
+% FRMS uses Parseval's theorem to calculate the RMS of a signal 
+% by examining the frequency domain of the signal.
+%
+% SYNTAX :
+%	- [rms] = frms(S)
+%
+% INPUT :
+%	- S: is the signal in the frequency domain (i.e. fft(s(t))). 
+%
+% OUTPUT:
+%	- rms: root-mean-square value of the signal.
+% ===============================
     
     rms = sqrt(sum((abs(S)/length(S)).^2));
 
