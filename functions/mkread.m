68c729fa529cd5cf08499bf09f8681fd1c1f9712
Bels-coder <78752329+Bels-coder@users.noreply.github.com>
2022-04-08 15:53:18 +0200

    Add files via upload
    added mkread.m to upload metakernel

---
+++ functions/mkread.m
@@ -0,0 +1,49 @@
+function mkread(mk)
+
+cspice_furnsh(mk)
+%{
+[d,f,e]=fileparts(mk);
+% C=pwd;
+% cd (d)
+
+ 
+fid=fopen(mk,'rt');
+mk=strrep(mk,'.mk','.abs');
+mk=strrep(mk,'.tm','.abs');
+fod=fopen(mk,'w');
+
+tmp = textscan(fid,'%s','Delimiter','\n');
+fclose(fid);
+
+tmp=tmp{1}
+% fclose all;
+% cspice_kclear
+% disp('resetting al kernels')
+% PATH_VALUES='NAN';
+for i=1:numel(tmp)
+    if any(strfind(strrep(tmp{i},' ',''),'PATH_VALUES='));
+%         disp([tmp{i} ';'])
+        eval([tmp{i} ';'])
+        
+        if any(strfind(PATH_VALUES,'..'))
+            % e' un path relativo
+            abs=[d filesep PATH_VALUES];
+        else
+            abs=PATH_VALUES;
+        end
+%         cd(PATH_VALUES)
+%         abs=pwd;
+        tmp{i}=strrep(tmp{i},PATH_VALUES,abs);
+    end
+    
+%     if any(strfind(tmp{i},'PATH_SYMBOLS'));
+%         eval(tmp{i})
+%     end
+    
+    fprintf(fod,' %s \n',tmp{i});
+    
+end
+fclose (fod);
+% cd(C)
+kread(mk)
+%}
