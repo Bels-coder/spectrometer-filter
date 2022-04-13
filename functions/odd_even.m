13335dbd04508a5b2617938cfb7182c1adf1c650
Bels-coder <78752329+Bels-coder@users.noreply.github.com>
2022-04-13 11:11:23 +0200

    Comments on odd_even.m
    Added comments for potential improvements and bug fixes

--- functions/odd_even.m
+++ functions/odd_even.m
@@ -14,8 +14,28 @@ function spe_out = odd_even(spe_in)
 %	- spe_out: spectra corrected for the odd-even effect affecting all JIRAM spectra.
 % ===============================
 
-wvl = dlmread('JIRAM_WVL_00.txt');         % JIRAM bands                         
+wvl = dlmread('JIRAM_WVL_00.txt');         % JIRAM bands  
+% attenzione al path di dlmread, potrebbe non trovare il file da leggere. 
+% Per ovviare a questa cosa puoi usare i path assoluti, che puoi includere in ogni funzione oppure aggiungere un argomento opzionale che e' il percorso radice.
+% cerco di scrivere un esempio qui
+%
+% function spe_out = odd_even(spe_in, path)
+%
+% if nargin < 2  %nargin e' "number of argument input", cioe' il numero di argomenti di input della funzione
+% 	rootpath = '/home/chiara/documents/matlab/';
+% else
+%	rootpath = path;
+% end
+
 oe = 1:1:size(wvl); 
+% se non specifichi l'incremento, matlab assume 1, quindi puoi scrivere oe = 1:size(wvl). Questo vale anche per i cicli for.
+% occhio che size(wvl) in generale non e' uno scalare, e infatti in questo caso e' un array 336x1. Se non ha errori o problemi penso sia un caso,
+% e' probabile che prenda come ultimo valore di oe il primo elemento di size(wvl). Alternative a questo hai
+% - size(wvl,1)
+% - length(wvl), che e' uguale a max(size(wvl)), quindi ti da la massima dimensione di wvl.
+% - numel(wvl) che e' il numero di elementi di wvl.
+% In alcuni casi sono equivalenti alcune di queste scritture, ma non sempre.
+
 par = oe(mod(oe,2)~=1);                    % even spectral points
 dis = oe(mod(oe,2)~=0);                    % odd spectral points
 
@@ -26,6 +46,9 @@ spe_out = nan(size(spe_in));
 
 for i = 1:1:sp_dim
     if isnan(spe_in(i,:))
+    	% ATTENZIONE! isnan(spe_in(i,:)) e' un'array di lunghezza wl_dim, e non e' ovvio come if interpreti un array.
+	% Facendo un paio di esperimenti mi pare che interpreti isnan(spe_in(i,:)) = 1 solo se tutti gli elementi dell'array sono nan, altrimenti = 0.
+	% In caso, per rendere piu' sicuro il tutto puoi usare le funzioni "any" e "all" a seconda di cosa vuoi.
 	   disp('Skip')	          
 	   continue
     end
